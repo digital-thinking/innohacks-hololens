a36ba9523f50a9c6be75f0d76f52f047e8f8edea
Chris <iniquitous1988@gmail.com>
2016-06-12 03:05:48 +0200

    Spawn script

---
+++ unity-project/Assets/scripts/CalculateLocalBounds.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CalculateLocalBounds : MonoBehaviour {
+
+    void Start()
+    {
+        Debug.Log(calculateLocalBounds());
+    }
+
+    private Bounds calculateLocalBounds()
+    {
+        Quaternion currentRotation = this.transform.rotation;
+        this.transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+
+        Bounds bounds = new Bounds(this.transform.position, Vector3.zero);
+
+        foreach (Renderer renderer in GetComponentsInChildren<Renderer>())
+        {
+            bounds.Encapsulate(renderer.bounds);
+        }
+
+        Vector3 localCenter = bounds.center - this.transform.position;
+        bounds.center = localCenter;
+        
+        this.transform.rotation = currentRotation;
+        return bounds;
+    }
+}
---
+++ unity-project/Assets/scripts/SpawnPOI.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnPOI : MonoBehaviour {
+
+    public GameObject target;
+
+    public static float centerX = 8.411f;
+    public static float centerY = 49.0138f;
+    public static float width = 0.0192f;
+    public static float height = 0.0046f;
+
+
+    // Use this for initialization
+    void Spawn (float x, float y) {
+        float xnorm = (x - centerX) / (width);
+        float ynorm = (y - centerY) / (height);
+
+        Debug.Log("Xnorm:" + xnorm + " Ynorm:" + ynorm);
+
+        GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+        cube.transform.parent = target.transform;
+        cube.transform.localScale =(new Vector3(0.1f, 0.1f, 0.1f));
+        cube.transform.position = new Vector3(xnorm, gameObject.transform.position.y, ynorm);
+    }
+
+	
+	// Update is called once per frame
+	void Start () {
+        Spawn(8.3941f, 49.0069f);
+        Spawn(8.4133f, 49.0069f);
+        Spawn(8.3941f, 49.0115f);
+        Spawn(8.4133f, 49.0115f);
+    }
+}
